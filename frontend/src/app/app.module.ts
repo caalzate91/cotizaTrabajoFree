d134a2c3fd9fe658de30b0636332a48f9d1f76cf
Camilo Alzate <camilo.alzate@sofka.com.co>
2023-06-21 08:46:04 -0500

    Creacion de entidades con sus modelos y componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { WorkOrderComponent } from './work-order/work-order.component';
+import { BillComponent } from './bill/bill.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    WorkOrderComponent,
+    BillComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
