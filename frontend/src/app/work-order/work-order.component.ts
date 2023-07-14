fbae61450cc188118595247316de59a5ff000104
Camilo Alzate <caalzateg@gmail.com>
2023-07-13 23:52:24 -0500

    Prueba con jsPDF

--- frontend/src/app/work-order/work-order.component.ts
+++ frontend/src/app/work-order/work-order.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild  } from '@angular/core';
+import * as jspdf from "jspdf";
+import jsPDF from "jspdf";
+
+//import * as pdfMake from "pdfmake/build/pdfmake";
+//import * as pdfFonts from "pdfmake/build/vfs_fonts";
+//const htmlToPdfmake = require("html-to-pdfmake");
+//(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-work-order',
@@ -6,6 +13,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-order.component.css']
 })
 export class WorkOrderComponent {
+
+  @ViewChild('pdfTable', {static: false}) public pdfTable!: ElementRef;
+
 
   titleWorkOrderCustom : String = "Bienvenido a la creacion de orden de servicio"
   nameSourceUser : String = ""
@@ -16,7 +26,33 @@ export class WorkOrderComponent {
   conceptName : String = ""
   conceptValue : String = ""
 
-  ngOnInit(): void {
+  /*public exportPDF() {
+    console.log("Hola")
+    const pdfTable = this.pdfTable.nativeElement;
+    console.log(pdfTable)
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).download();
+
+  }*/
+
+  ngAfterViewInit(){
+  }
+
+
+  exportPDF() {
+    console.log("Hola")
+    const container = this.pdfTable.nativeElement;
+
+    let pdf = new jsPDF()
+
+    pdf.html(container, {
+      callback: (pdf) => {
+        pdf.save("sample.pdf")
+      }
+    }
+    )
+
   }
 
 }
